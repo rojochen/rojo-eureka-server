161a74b6cb63b7d0a92af805795099c545b45a8b
rogerchen <jjc.roger@gmail.com>
2017-10-12 03:18:23 +0800

    first commit

---
+++ src/main/java/rojo/lab/eureka/MsEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package rojo.lab.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class MsEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEurekaServerApplication.class, args);
+	}
+}
