25ed5f9515a1bcd9aee01237f2ccced813d4b845
rojochen <jjc.roger@gmail.com>
2017-10-17 11:00:07 +0800

    changed application to yml file

--- src/main/java/rojo/lab/eureka/RojoEurekaServerApplication.java
+++ src/main/java/rojo/lab/eureka/RojoEurekaServerApplication.java
@@ -2,12 +2,14 @@ package rojo.lab.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
-public class MsEurekaServerApplication {
+public class RojoEurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsEurekaServerApplication.class, args);
+		SpringApplication.run(RojoEurekaServerApplication.class, args);
 	}
 }
--- src/test/java/rojo/lab/eureka/RojoEurekaServerApplicationTests.java
+++ src/test/java/rojo/lab/eureka/RojoEurekaServerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MsEurekaServerApplicationTests {
+public class RojoEurekaServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
